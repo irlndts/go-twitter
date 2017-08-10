b7fa94104e2b6e114f6a2b982402c4653e460bd8
irlndts <kleto4kin@gmail.com>
2017-08-10 09:45:44 +0300

    Init commit

---
+++ examples/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"githab.com/irlndts/go-twitter"
+)
+
+func main() {
+
+}
---
+++ twitter.go
@@ -0,0 +1,11 @@
+package twitter
+
+const twitterAPI = "https://api.twitter.com/1.1/"
+
+// Client ...
+type Client struct{}
+
+// NewClient creates new twitter client
+func NewClient() *Client {
+
+}
