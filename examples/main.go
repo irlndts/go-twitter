f4558d39d5caca0699e2ba72dc281d82314e7f3c
irlndts <kleto4kin@gmail.com>
2017-08-19 13:15:16 +0300

    Status

--- examples/main.go
+++ examples/main.go
@@ -3,8 +3,11 @@ package main
 import "github.com/irlndts/go-twitter"
 
 func main() {
+	consumer := ""
+	secret := ""
 	twitter := twitter.NewTwitterClient(consumer, secret)
-	twitter.Login()
+	//twitter.Login()
+	twitter.Update("TEST")
 	/*
 		twitter, err := twitter.Auth(consumer, secret)
 		if err != nil {
--- twitter.go
+++ twitter.go
@@ -21,6 +21,9 @@ const (
 	twitterAPI             = "https://api.twitter.com/1.1/"
 	twitterAuth            = "https://api.twitter.com/oauth2/token"
 	twitterRequestTokenURL = "https://api.twitter.com/oauth/request_token"
+	twitterAccessTokenURL  = "https://api.twitter.com/oauth/access_token"
+
+	twitterStatusesUpdate = "https://api.twitter.com/1.1/statuses/update.json"
 
 	contentType = "application/x-www-form-urlencoded;charset=UTF-8"
 	userAgent   = "go-twitter v0.1"
@@ -69,7 +72,10 @@ func (t *Twitter) OauthAuthorizationHeader(method, uri string) string {
 	v.Set("oauth_consumer_key", t.oauth.ConsumerKey)
 	v.Set("oauth_signature_method", t.oauth.SignatureMethod)
 	v.Set("oauth_version", t.oauth.Version)
-	v.Set("oauth_callback", "oob")
+	v.Set("oauth_token", t.oauth.Token)
+	//v.Set("oauth_callback", "oob")
+	//v.Set("oauth_verifier", "5555915")
+	v.Set("status", "TEST")
 	parameterString := v.Encode()
 
 	signatureBaseString := strings.Join([]string{
@@ -79,6 +85,9 @@ func (t *Twitter) OauthAuthorizationHeader(method, uri string) string {
 	}, "&")
 
 	signingKey := url.QueryEscape(t.oauth.ConsumerSecret) + "&"
+	if t.oauth.TokenSecret != "" {
+		signingKey = signingKey + url.QueryEscape(t.oauth.TokenSecret)
+	}
 
 	mac := hmac.New(sha1.New, []byte(signingKey))
 	mac.Write([]byte(signatureBaseString))
@@ -91,9 +100,76 @@ func (t *Twitter) OauthAuthorizationHeader(method, uri string) string {
 		`oauth_signature="` + url.QueryEscape(oauth_signature) + `", ` +
 		`oauth_signature_method="` + url.QueryEscape(t.oauth.SignatureMethod) + `", ` +
 		`oauth_timestamp="` + url.QueryEscape(oauth_timestamp) + `", ` +
+		`oauth_token="` + url.QueryEscape(t.oauth.Token) + `", ` +
 		`oauth_version="` + url.QueryEscape(t.oauth.Version) + `"`
+	fmt.Println(authorization)
 
 	return authorization
+}
+
+func (t *Twitter) Update(status string) {
+
+	authorization := t.OauthAuthorizationHeader("POST", twitterStatusesUpdate)
+
+	body := strings.NewReader("status=" + status)
+
+	req, err := http.NewRequest("POST", twitterStatusesUpdate, body)
+	req.Header.Add("Authorization", authorization)
+	req.Header.Add("Content-Type", contentType)
+	req.Header.Add("User-Agent", userAgent)
+
+	client := &http.Client{Timeout: time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		m, err := url.ParseQuery(string(bodyBytes))
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	fmt.Println(string(bodyBytes))
+
+}
+
+func (t *Twitter) AccessToken(code string) {
+
+	authorization := t.OauthAuthorizationHeader("POST", twitterAccessTokenURL)
+	body := strings.NewReader("oauth_verifier=" + code)
+
+	req, err := http.NewRequest("POST", twitterAccessTokenURL, body)
+	req.Header.Add("Authorization", authorization)
+	req.Header.Add("Content-Type", contentType)
+	req.Header.Add("User-Agent", userAgent)
+
+	client := &http.Client{Timeout: time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		m, err := url.ParseQuery(string(bodyBytes))
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	fmt.Println(string(bodyBytes))
+
 }
 
 func (t *Twitter) Login() {
@@ -101,7 +177,6 @@ func (t *Twitter) Login() {
 	authorization := t.OauthAuthorizationHeader("POST", twitterRequestTokenURL)
 	body := strings.NewReader("oauth_callback=oob")
 
-	// step 1
 	req, err := http.NewRequest("POST", twitterRequestTokenURL, body)
 	req.Header.Add("Authorization", authorization)
 	req.Header.Add("Content-Type", contentType)
@@ -127,8 +202,8 @@ func (t *Twitter) Login() {
 	t.oauth.Token = m.Get("oauth_token")
 	t.oauth.TokenSecret = m.Get("oauth_token_secret")
 	t.oauth.CallbackConfirmed = m.Get("oauth_callback_confirmed")
-	fmt.Println(t)
 
+	fmt.Printf("%+v", t.oauth)
 }
 
 // AuthAOA registers in twitter API and returns bearer key
